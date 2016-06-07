fb175253f6f1c218a6b2a6ba196539fbae964c79
Dan Schlauch <dschlauch@gmail.com>
2016-06-07 10:41:17 -0400

    only display xkcd on MWF

--- public/js/xkcd.js
+++ public/js/xkcd.js
@@ -1,21 +1,28 @@
         
 	  var updateXKCD = function () {
-		$.ajax({
-		    url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
-		    dataType: "json",
-		    jsonpCallback: "xkcddata",
-		    success: function(data) {
-			$("#xkcd").append(
-			    $("<h1/>").text(data.title),
-			    $("<img/>").attr({
-				src: data.img,
-				title: data.alt,
-				alt: data.title,
-				width: "400px"
-			    })
-			);
+		    var d = new Date();
+		    var n = d.getDay()%2
+		    if (n==1){
+			$.ajax({
+				url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
+				dataType: "json",
+				jsonpCallback: "xkcddata",
+				success: function(data) {
+					$("#xkcd").append(
+					    $("<h1/>").text(data.title),
+					    $("<img/>").attr({
+						src: data.img,
+						title: data.alt,
+						alt: data.title,
+						width: "400px"
+					    })
+					);
+				}
+			});
+		    } else {
+			document.getElementById("xkcd").innerHTML = "no";
 		    }
-		});
+
 
         };
         $(document).ready(function() {
